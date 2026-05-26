7480f19564b8f8a3be10242d8fb4c7f2c54f869e
Arnold Wilson Fernandes <arnoldfern.1945@gmail.com>
2026-05-27 00:01:27 +0530

    Fixing the website URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+//added base
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/MyPortfolio/",
